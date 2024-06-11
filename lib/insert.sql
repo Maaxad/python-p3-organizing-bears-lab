3ff7282f87a2137ea33e0c9883a94bbfa0c3d29a
mahad <muhumedmohamed836@gmail.com>
2024-06-11 21:06:34 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+('Rowdy', 3, 'M', 'Black', 'Playful', 1),
+('Tabitha', 7, 'F', 'Gray', 'Calm', 1),
+('Sergeant Brown', 8, 'M', 'Brown', 'Grumpy', 1),
+('Melissa', 4, 'F', 'White', 'Energetic', 1),
+('Grinch', 6, 'M', 'Green', 'Sneaky', 0),
+('Wendy', 9, 'F', 'Brown', 'Sweet', 1),
+(NULL, NULL, NULL, NULL, NULL, NULL); -- for the unnamed bear
--- lib/seed.sql
+++ lib/seed.sql
@@ -7,3 +7,35 @@ INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
 (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
 (7,"Wendy", 6, "F", "Blue", "naive", 1),
 (8,null, 20, "M", "black", "aggressive", 0);
+
+select_all_female_bears_return_name_and_age = """
+    SELECT
+        name,
+        age
+    FROM bears
+    WHERE sex = 'F';
+"""
+
+select_all_brown_bears_return_name_and_age = """
+    SELECT
+        name,
+        age
+    FROM bears
+    WHERE color = 'Brown';
+"""
+
+select_all_bears_return_name_and_age_alive = """
+    SELECT
+        name,
+        age,
+        alive
+    FROM bears;
+"""
+
+select_all_bears_return_name_and_age_sorted_by_age = """
+    SELECT
+        name,
+        age
+    FROM bears
+    ORDER BY age;
+"""
